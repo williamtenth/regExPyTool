f31ffb3cc9298677c1bf6c8494d1a9a10340df2a
williamtenth <williamtenth@gmail.com>
2022-07-25 22:44:56 -0500

    .gitignore is now working

--- export2Regex.py
+++ export2Regex.py
@@ -4,11 +4,13 @@ import re
 import sys
 
 file = sys.argv[1]
+stringPattern = sys.argv[2]
 with open(file) as f:
 	for line in f:#lines:
 		if "Service" not in line:
 			continue
-		pattern = r"_(\d+)_(\d+)_(\d+)"
+		pattern = re.compile(stringPattern)
+		# pattern for example _(\d+)_(\d+)_(\d+)
 		#print(pattern)
 		result = re.search(pattern, line)
-		print(result[1]+" "+result[2]+" "+result[3])
+		print(result.group())
